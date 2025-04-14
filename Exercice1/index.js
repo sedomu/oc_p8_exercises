4ef08522ad2f0659aebe74af93af0aa41243a2d6
sedomu <seguin@lilo.org>
2025-04-14 21:19:22 +0200

    exercises 1 & 2

--- Exercice1/index.js
+++ Exercice1/index.js
@@ -1,3 +1,23 @@
 //votre code ici
+function pairNumbers(start, end){
+    let result = [];
 
+    for (let i = start; i <= end; i++){
+        i % 2 === 0 ? result.push(i) : "";
+    }
+
+    if (result.length === 0){
+        return "";
+    }
+
+    let resultString = "";
+
+    for (let i = 0; i < result.length - 1; i++){
+        resultString += result[i].toString();
+        resultString += ",";
+    }
+    resultString += result[result.length - 1];
+
+    return resultString;
+}
 export default pairNumbers
